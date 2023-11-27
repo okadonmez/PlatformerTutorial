9878d8352880e02b1215bcdeb11959cff67e5bac
Oguz Kagan Donmez <oguzkagandonmez01@gmail.com>
2023-11-27 11:03:31 +0300

    04. Bolum

--- src/Inputs/MouseInputs.java
+++ src/Inputs/MouseInputs.java
@@ -23,7 +23,7 @@ public class MouseInputs implements MouseListener, MouseMotionListener {
 	@Override
 	public void mouseMoved(MouseEvent e) {
 		System.out.println("Mouse moved!");
-		//gamePanel.setRectPos(e.getX(), e.getY());
+		gamePanel.setRectPos(e.getX(), e.getY());
 	}
 
 	@Override
--- src/main/GamePanel.java
+++ src/main/GamePanel.java
@@ -1,9 +1,12 @@
 package main;
 
-import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.Graphics;
-import java.util.Random;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.io.InputStream;
 
+import javax.imageio.ImageIO;
 import javax.swing.JPanel;
 
 import Inputs.KeyboardInputs;
@@ -13,17 +16,37 @@ public class GamePanel extends JPanel {
 
 	private MouseInputs mouseInputs;
 	private float xDelta = 100, yDelta = 100;
-	private float xDir = 1f, yDir = 1f;
-	private Color color = new Color(150, 20, 90);
-	private Random random;
-	
+	private BufferedImage img, subImg;
+
 	public GamePanel() {
-		random = new Random();
+
 		mouseInputs = new MouseInputs(this);
+
+		importImp();
+
+		setPanelSize();
 		addKeyListener(new KeyboardInputs(this));
 		addMouseListener(mouseInputs);
 		addMouseMotionListener(mouseInputs);
 
+	}
+
+	private void importImp() {
+		InputStream is = getClass().getResourceAsStream("/player_sprites.png");
+
+		try {
+			img = ImageIO.read(is);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	private void setPanelSize() {
+		Dimension size = new Dimension(1280, 800);
+		setMinimumSize(size);
+		setPreferredSize(size);
+		setMaximumSize(size);
 	}
 
 	public void ChangeXDelta(int value) {
@@ -42,30 +65,7 @@ public class GamePanel extends JPanel {
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
 
-		updateRectangle();
-		g.setColor(color);
-		g.fillRect((int) xDelta, (int) yDelta, 200, 50);
-	}
-
-	private void updateRectangle() {
-		xDelta += xDir;
-		if (xDelta > 400 || xDelta < 0) {
-			xDir *= -1;
-			color = getRndColor();
-		}
-
-		yDelta += yDir;
-		if (yDelta > 400 || yDelta < 0) {
-			yDir *= -1;
-			color = getRndColor();
-		}
-	}
-	
-	private Color getRndColor() {
-		int r = random.nextInt(255);
-		int g = random.nextInt(255);
-		int b = random.nextInt(255);
-		
-		return new Color(r, g, b);
+		subImg = img.getSubimage(1 * 64, 8 * 40, 64, 40);
+		g.drawImage(subImg, (int)xDelta, (int)yDelta, 128, 80, null);
 	}
 }
