5ccc59afc80520ba028492298ddd21be7e28fc76
Oguz Kagan Donmez <oguzkagandonmez01@gmail.com>
2024-02-05 21:48:11 +0300

    16. Bolum

--- src/entities/Player.java
+++ src/entities/Player.java
@@ -3,9 +3,12 @@ package entities;
 import static utilz.Constants.PlayerConstants.*;
 import static utilz.HelpMethods.*;
 
+import java.awt.Color;
 import java.awt.Graphics;
+import java.awt.geom.Rectangle2D;
 import java.awt.image.BufferedImage;
 
+import gamestates.Playing;
 import main.Game;
 import utilz.LoadSave;
 
@@ -28,22 +31,107 @@ public class Player extends Entity {
 	private float fallSpeedAfterCollision = 0.5f * Game.SCALE;
 	private boolean inAir = false;
 
-	public Player(float x, float y, int width, int height) {
+	// StatusBarUI
+	private BufferedImage statusBarImg;
+
+	private int statusBarWidth = (int) (192 * Game.SCALE);
+	private int statusBarHeight = (int) (58 * Game.SCALE);
+	private int statusBarX = (int) (10 * Game.SCALE);
+	private int statusBarY = (int) (10 * Game.SCALE);
+
+	private int healthBarWidth = (int) (150 * Game.SCALE);
+	private int healthBarHeight = (int) (4 * Game.SCALE);
+	private int healthBarXStart = (int) (34 * Game.SCALE);
+	private int healthBarYStart = (int) (14 * Game.SCALE);
+
+	private int maxHealth = 100;
+	private int currentHealth = maxHealth;
+	private int healthWidth = healthBarWidth;
+
+	// AttackBox
+	private Rectangle2D.Float attackBox;
+
+	private int flipX = 0;
+	private int flipW = 1;
+
+	private boolean attackChecked;
+	private Playing playing;
+
+	public Player(float x, float y, int width, int height, Playing playing) {
 		super(x, y, width, height);
+
+		this.playing = playing;
+
 		loadAnimations();
 		initHitbox(x, y, (int) (20 * Game.SCALE), (int) (27 * Game.SCALE));
+		initAttackBox();
+	}
+
+	private void initAttackBox() {
+		attackBox = new Rectangle2D.Float(x, y, (int) (20 * Game.SCALE), (int) (20 * Game.SCALE));
 	}
 
 	public void update() {
+		updateHealthBar();
+		
+		if(currentHealth <= 0) {
+			playing.setGameOver(true);
+			
+			return;
+		}
+		
+		updateAttackBox();
 		updatePos();
+
+		if (attacking) {
+			checkAttack();
+		}
+
 		updateAnimationTick();
 		setAnimation();
 	}
 
+	private void checkAttack() {
+		if (attackChecked || aniIndex != 1) {
+			return;
+		}
+
+		attackChecked = true;
+		playing.checkEnemyHit(attackBox);
+	}
+
+	private void updateAttackBox() {
+		if (right) {
+			attackBox.x = hitbox.x + hitbox.width + (int) (Game.SCALE * 10);
+		} else if (left) {
+			attackBox.x = hitbox.x - hitbox.width - (int) (Game.SCALE * 10);
+		}
+
+		attackBox.y = hitbox.y + (Game.SCALE * 10);
+	}
+
+	private void updateHealthBar() {
+		healthWidth = (int) ((currentHealth / (float) maxHealth) * healthBarWidth);
+	}
+
 	public void render(Graphics g, int LvlOffset) {
-		g.drawImage(animations[playerAction][aniIndex], (int) (hitbox.x - xDrawOffset) - LvlOffset,
-				(int) (hitbox.y - yDrawOffset), width, height, null);
-		drawHitbox(g);
+		g.drawImage(animations[playerAction][aniIndex], (int) (hitbox.x - xDrawOffset) - LvlOffset + flipX,
+				(int) (hitbox.y - yDrawOffset), width * flipW, height, null);
+
+		drawHitbox(g, LvlOffset);
+		drawAttackBox(g, LvlOffset);
+		drawUI(g);
+	}
+
+	private void drawAttackBox(Graphics g, int LvlOffset) {
+		g.setColor(Color.red);
+		g.drawRect((int) attackBox.x - LvlOffset, (int) attackBox.y, (int) attackBox.width, (int) attackBox.height);
+	}
+
+	private void drawUI(Graphics g) {
+		g.drawImage(statusBarImg, statusBarX, statusBarY, statusBarWidth, statusBarHeight, null);
+		g.setColor(Color.red);
+		g.fillRect(healthBarXStart + statusBarX, healthBarYStart + statusBarY, healthWidth, healthBarHeight);
 	}
 
 	private void updateAnimationTick() {
@@ -56,6 +144,7 @@ public class Player extends Entity {
 			if (aniIndex >= GetSpriteAmount(playerAction)) {
 				aniIndex = 0;
 				attacking = false;
+				attackChecked = false;
 			}
 		}
 	}
@@ -78,7 +167,14 @@ public class Player extends Entity {
 		}
 
 		if (attacking) {
-			playerAction = ATTACK_1;
+			playerAction = ATTACK;
+
+			if (startAni != ATTACK) {
+				aniIndex = 1;
+				aniTick = 0;
+				
+				return;
+			}
 		}
 
 		if (startAni != playerAction) {
@@ -112,10 +208,14 @@ public class Player extends Entity {
 
 		if (left) {
 			xSpeed -= playerSpeed;
+			flipX = width;
+			flipW = -1;
 		}
 
 		if (right) {
 			xSpeed += playerSpeed;
+			flipX = 0;
+			flipW = 1;
 		}
 
 		if (!inAir) {
@@ -169,10 +269,21 @@ public class Player extends Entity {
 		}
 	}
 
+	public void changeHealth(int value) {
+		currentHealth += value;
+
+		if (currentHealth <= 0) {
+			currentHealth = 0;
+			// gameOver();
+		} else if (currentHealth >= maxHealth) {
+			currentHealth = maxHealth;
+		}
+	}
+
 	private void loadAnimations() {
 		BufferedImage img = LoadSave.GetSpriteAtlas(LoadSave.PLAYER_ATLAS);
 
-		animations = new BufferedImage[9][6];
+		animations = new BufferedImage[7][8];
 
 		for (int j = 0; j < animations.length; j++) {
 
@@ -180,6 +291,8 @@ public class Player extends Entity {
 				animations[j][i] = img.getSubimage(i * 64, j * 40, 64, 40);
 			}
 		}
+
+		statusBarImg = LoadSave.GetSpriteAtlas(LoadSave.STATUS_BAR);
 	}
 
 	public void loadLvlData(int[][] lvlData) {
@@ -235,5 +348,22 @@ public class Player extends Entity {
 
 	public void setJump(boolean jump) {
 		this.jump = jump;
+	}
+
+	public void resetAll() {
+		resetDirBooleans();
+		
+		inAir = false;
+		attacking = false;
+		moving = false;
+		playerAction = IDLE;
+		currentHealth = maxHealth;
+		
+		hitbox.x = x;
+		hitbox.y = y;
+		
+		if(!IsEntityOnFloor(hitbox, lvlData)) {
+			inAir = true;
+		}
 	}
 }
--- src/gamestates/Playing.java
+++ src/gamestates/Playing.java
@@ -4,12 +4,15 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.event.KeyEvent;
 import java.awt.event.MouseEvent;
+import java.awt.geom.Rectangle2D;
 import java.awt.image.BufferedImage;
 import java.util.Random;
 
+import entities.EnemyManager;
 import entities.Player;
 import levels.LevelManager;
 import main.Game;
+import ui.GameOverOverlay;
 import ui.PauseOverlay;
 import utilz.LoadSave;
 import static utilz.Constants.Environment.*;
@@ -18,7 +21,9 @@ public class Playing extends State implements Statemethods {
 
 	private Player player;
 	private LevelManager levelManager;
+	private EnemyManager enemyManager;
 	private PauseOverlay pauseOverlay;
+	private GameOverOverlay gameOverOverlay;
 	private boolean paused = false;
 
 	private int xLvlOffset;
@@ -31,6 +36,8 @@ public class Playing extends State implements Statemethods {
 	private BufferedImage backgroundImg, bigCloud, smallCloud;
 	private int[] smallCloudsPos;
 	private Random rnd = new Random();
+
+	private boolean gameOver;
 
 	public Playing(Game game) {
 		super(game);
@@ -50,16 +57,19 @@ public class Playing extends State implements Statemethods {
 
 	private void initClasses() {
 		levelManager = new LevelManager(game);
-		player = new Player(200, 200, (int) (64 * Game.SCALE), (int) (40 * Game.SCALE));
+		enemyManager = new EnemyManager(this);
+		player = new Player(200, 200, (int) (64 * Game.SCALE), (int) (40 * Game.SCALE), this);
 		player.loadLvlData(levelManager.getCurrentLevel().getLevelData());
 		pauseOverlay = new PauseOverlay(this);
+		gameOverOverlay = new GameOverOverlay(this);
 	}
 
 	@Override
 	public void update() {
-		if (!paused) {
+		if (!paused && !gameOver) {
 			levelManager.update();
 			player.update();
+			enemyManager.update(levelManager.getCurrentLevel().getLevelData(), player);
 			checkCloseToBorder();
 		} else {
 			pauseOverlay.update();
@@ -91,11 +101,14 @@ public class Playing extends State implements Statemethods {
 
 		levelManager.draw(g, xLvlOffset);
 		player.render(g, xLvlOffset);
+		enemyManager.draw(g, xLvlOffset);
 
 		if (paused) {
 			g.setColor(new Color(0, 0, 0, 150));
 			g.fillRect(0, 0, Game.GAME_WIDTH, Game.GAME_HEIGHT);
 			pauseOverlay.draw(g);
+		} else if (gameOver) {
+			gameOverOverlay.draw(g);
 		}
 	}
 
@@ -111,70 +124,102 @@ public class Playing extends State implements Statemethods {
 		}
 	}
 
+	public void resetAll() {
+		// TODO: reset playing, enemy, lvl etc.
+		gameOver = false;
+		paused = false;
+		player.resetAll();
+		enemyManager.resetAllEnemies();
+	}
+
+	public void setGameOver(boolean gameOver) {
+		this.gameOver = gameOver;
+	}
+
+	public void checkEnemyHit(Rectangle2D.Float attackBox) {
+		enemyManager.checkEnemyHit(attackBox);
+	}
+
 	@Override
 	public void mouseClicked(MouseEvent e) {
-		if (e.getButton() == MouseEvent.BUTTON1) {
-			player.setAttacking(true);
+		if (!gameOver) {
+			if (e.getButton() == MouseEvent.BUTTON1) {
+				player.setAttacking(true);
+			}
 		}
 	}
 
 	public void mouseDragged(MouseEvent e) {
-		if (paused) {
-			pauseOverlay.mouseDragged(e);
+		if (!gameOver) {
+			if (paused) {
+				pauseOverlay.mouseDragged(e);
+			}
 		}
 	}
 
 	@Override
 	public void mousePressed(MouseEvent e) {
-		if (paused) {
-			pauseOverlay.mousePressed(e);
+		if (!gameOver) {
+			if (paused) {
+				pauseOverlay.mousePressed(e);
+			}
 		}
 	}
 
 	@Override
 	public void mouseReleased(MouseEvent e) {
-		if (paused) {
-			pauseOverlay.mouseReleased(e);
+		if (!gameOver) {
+			if (paused) {
+				pauseOverlay.mouseReleased(e);
+			}
 		}
 	}
 
 	@Override
 	public void mouseMoved(MouseEvent e) {
-		if (paused) {
-			pauseOverlay.mouseMoved(e);
+		if (!gameOver) {
+			if (paused) {
+				pauseOverlay.mouseMoved(e);
+			}
 		}
 	}
 
 	@Override
 	public void keyPressed(KeyEvent e) {
-		switch (e.getKeyCode()) {
-		case KeyEvent.VK_A:
-			player.setLeft(true);
-			break;
-		case KeyEvent.VK_D:
-			player.setRight(true);
-			break;
-		case KeyEvent.VK_SPACE:
-			player.setJump(true);
-			break;
-		case KeyEvent.VK_ESCAPE:
-			paused = !paused;
-			break;
+		if (gameOver) {
+			gameOverOverlay.KeyPressed(e);
+		} else {
+			switch (e.getKeyCode()) {
+			case KeyEvent.VK_A:
+				player.setLeft(true);
+				break;
+			case KeyEvent.VK_D:
+				player.setRight(true);
+				break;
+			case KeyEvent.VK_SPACE:
+				player.setJump(true);
+				break;
+			case KeyEvent.VK_ESCAPE:
+				paused = !paused;
+				break;
+			}
 		}
 	}
 
 	@Override
 	public void keyReleased(KeyEvent e) {
-		switch (e.getKeyCode()) {
-		case KeyEvent.VK_A:
-			player.setLeft(false);
-			break;
-		case KeyEvent.VK_D:
-			player.setRight(false);
-			break;
-		case KeyEvent.VK_SPACE:
-			player.setJump(false);
-			break;
+		if (!gameOver) {
+			switch (e.getKeyCode()) {
+			case KeyEvent.VK_A:
+				player.setLeft(false);
+				break;
+			case KeyEvent.VK_D:
+				player.setRight(false);
+				break;
+			case KeyEvent.VK_SPACE:
+				player.setJump(false);
+				break;
+			}
 		}
 	}
 
